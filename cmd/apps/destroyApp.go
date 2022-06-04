51224d3c5bf50df9ba117d72d53009f525dbc941
Troy Coll <troy.coll@gmail.com>
2022-06-04 17:08:26 -0400

    destroy app

---
+++ cmd/apps/destroyApp.go
@@ -0,0 +1,23 @@
+package apps
+
+import "github.com/spf13/cobra"
+
+var destroyAppCmd = &cobra.Command{
+	Short: "destroys a Heroku app",
+	Long:  "destroys a Heroku app",
+	Use:   "destroy",
+	Run:   destroyApp,
+}
+
+// OPTIONS
+//   -a, --app=app          app to run command against
+//   -c, --confirm=confirm
+//   -r, --remote=remote    git remote of app to use
+
+func init() {
+	AppsCmd.AddCommand(destroyAppCmd)
+}
+
+func destroyApp(cmd *cobra.Command, args []string) {
+	loadDefaultVariables()
+}
